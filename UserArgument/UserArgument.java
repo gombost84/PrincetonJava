19c596da51cf918075cff841d2e95f54b2afd5e7
gombost84 <tamas.gombos84@gmail.com>
2023-06-24 00:36:43 +0200

    First commit

---
+++ UserArgument/UserArgument.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package userargument;
+
+/**
+ *
+ * @author gombost84
+ */
+public class UserArgument {
+
+    public static void main(String[] args) {
+        System.out.print("Hi, ");
+        System.out.print(args[0]);
+        System.out.println(". How are you?");
+    }
+
+}
